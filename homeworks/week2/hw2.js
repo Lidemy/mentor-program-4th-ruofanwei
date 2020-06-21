1782c74abac9ef04814627d45d2a1de1c24af2d4
tina <px732amgo81sp8@gmail.com>
2020-06-21 18:22:30 +0800

    week2 修正

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,12 @@
 function capitalize(str) {
-  
+  var answer = ''
+  if (str[0] >= 'a' && str[0] <= 'z') {
+      answer = str[0].toUpperCase()
+  } else answer = str[0]
+  for (var i = 1; i < str.length; i++) {
+      answer += str[i]
+  }
+  return answer
 }
 
 console.log(capitalize('hello'));
